5f275500c7abe1a8cae1aa97dfd643f6eacfafe2
captaingector <gector@gectors.email>
2018-02-12 12:51:43 -0800

    Turned ioMod.py into a object-based system

--- ioMod.py
+++ ioMod.py
@@ -1,6 +1,24 @@
 import json
 
 ## module for handing IO for the scratchy discord bot
+class json_handler():
+	def __init__(self):
+		self.foo = "Hello,"
+
+	## Demonstrate using self values in other places
+	def foo_method(self, bar):
+		print(self.foo + bar)
+	
+	def read(self, fp):
+		with open(fp) as json_file:
+			return json.load(json_file)
+	def write(self, obj, fb):	
+		with open(fb, 'w') as json_file:
+			return json.dump(obj, json_file, indent=4)
+	def make_backups(files=None):
+	
+
+'''
 def read_json(f):
 	with open(f) as json_file: 
 		data = json.load(json_file)
@@ -13,13 +31,20 @@ def write_json(obj, fp):
 		print("writing: ")
 		print(obj)
 		json.dump(obj, json_file, indent=4)
-		
+	
 def make_backups():
 	print("Making a backup of all .json files...")
 	print("Done.")
+'''
 
 def test(s):
 	print(s)	
+
+if __name__ == "__main__":
+	print("We are main.")
+	handle = json_handler()
+	handle.foo_method(" world!");
+	
 #def write_file(f, string):
 	
 
--- test.py
+++ test.py
@@ -20,6 +20,22 @@ data_to_json = {
 	"scratchy": data1,
 	"eet139": data2
 }
-ioMod.test("Hello, World!")
-test = ioMod.read_json("data.json")
-ioMod.write_json(data_to_json, "data2.json")
+
+'''
+	I intent to make the json_handler useful in such a way that you will make a new instance of the json_handler for every user. 
+Ideally, the json handler would smartly handle the user files in a non-destructive fashion, I'm going to try and make it automatically save/backup
+files when they are opened/closed, and since I believe there is an opposite to the __init__ function, it should be possible to execute code when 
+the program is stopped. Eg, saving any unsaved changes.
+'''
+handler = ioMod.json_handler() # make a new json_handler object
+
+ioMod.test("Hello, World!") # Demonstration of calling a function that's part of a module
+handler.foo_method(" world") # Demonstration of calling a function that's part of a class/object
+
+print(handler.read("data2.json")) # Read
+handler.write(data_to_json, "data2.json") # Write
+
+# The thing is, the json libary "dump" only returns None or it fails. A smart definition would return ether success or an error, but do it in such a way
+# 	that it wouldn't break the program. In rust, this is done with a vlaue "pair", which I might end up implementing for this. Alnternatively, for 
+# 	failures we can use the built-in error catching, while not as elegant imo, still works great.
+
