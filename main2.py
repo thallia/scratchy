ef2cbb81cd021ccd3bcad5bb7e948c2fe913fa69
thallia <thalliatree@gmail.com>
2018-02-19 22:00:16 -0800

    new grab written - untested

--- main2.py
+++ main2.py
@@ -12,14 +12,8 @@ from helpMeh import help_meh
 description = "scratchpad bot"
 path = "/home/thallia/code/scratchy/"
 file_token = "/home/thallia/key/scratchy-discord-token.txt"
-<<<<<<< HEAD
 #if os.getcwd().find("gector"):
     #file_token = "/home/gector/key.txt"
-=======
-prefix = "@"
-if os.getcwd().find("gector"):
-    file_token = "/home/gector/key.txt"
->>>>>>> f212b8f9aa5f66a55e31e9644a527a95340a09d7
 
 token = ""
 
@@ -46,71 +40,13 @@ def find_quote(msg_list, index = 0):
         if x.find('"') != -1:
             return msg_list.index(x)
 
-def set_entry(args, data = None):
-    # set <entry> <field> <value>
-    
-    value = args[2:]  
-
-    index1 = find_quote(value)
-    index2 = find_quote(value, index1+1)
-
-    print(value[index1+1:])
-    print("Index1: {}, index2: {}".format(index1, index2))
-    print("Arg0: {}, Arg1: {}".format(args[0], args[1]))   
-    text = None
-    if index2 != None:
-        text = ' '.join(value[index1:index2+1]).replace('"', "")
-    else:
-        text = ' '.join(value[index1:]).replace('"', "")
-
-    print(text)
-    
-    # Make a temporary backup.
-    handler = ioMod.json_handler()
-    handler.make_backup()
-
-    if args[1] != "values":
-        try:
-            data[args[0]][args[1]] = text
-            # write_user(obj, user)
-            handler.write_user(data, msg_author)
-        except KeyError:
-            return "Entry not in scratchpad!"
-    elif args[1] == "values":
-        try:
-            data[args[0]][args[1]].insert(0, text)
-            handler.write_user(data, msg_author)
-        except KeyError:
-            return "If you see this, check set function for idiot's coding error."
-
-    return "Success! Try `{}show {}` to display your new scratchpad entry".format(prefix, args[0])
-
-def new(data=None, arg=None):
-    handler = ioMod.json_handler()
-    try:
-        data = handler.read_user(msg_author)
-        if data == None:
-            # new_file(user, fp = None)
-            handler.new_file(msg_author)
-            data = handler.read_user(msg_author)
-    
-        # new(user, entry_name):
-        handler.new(msg_author, arg)
-        return "Success! Try `{}show {}` to display your new scratchpad entry".format(prefix, arg)
-    except Exception as e:
-        if debug:
-            return "Error: {}".format(e);
-        else:
-            return "Unable to complete request! Please contact a developer."
-
-
-def show(data=None, subject = None, values = None):
+def show(data=None, subject = None):
     show_all = False;
     fill_me = ""
     if subject != None and subject != []:
         if set(subject).issubset(data):
             data = {key:data[key] for key in subject}
-            #print("Data: {}, \nSubject: {}".format(data, subject))
+            print("Data: {}, \nSubject: {}".format(data, subject))
             show_all = True;
             fill_me = ""
         else:
@@ -129,16 +65,9 @@ def show(data=None, subject = None, values = None):
                 elif type(value) == list:
                     indent = 6*' '
                     fill_me += element + ": " + "\n" + indent
-                    if values == None:
-                        for x in range(0, 9):
-                            if x < len(value) and value[x] != None:
-                                fill_me += "({}) ".format(x) + "\"" + value[x] + "\"\n" + indent
-                    else:
-                        for x in values:
-                            if x < len(value) and value[x] != None:
-                                fill_me += "({}) ".format(x) + "\"" + value[x] + "\"\n" + indent
-
-
+                    for x in range(0, 9):
+                        if x < len(value) and value[x] != None:
+                            fill_me += "({}) ".format(x) + "\"" + value[x] + "\"\n" + indent
         elif show_all == False:
             fill_me = "```\n"
             for f in data.values():
@@ -152,32 +81,7 @@ def show(data=None, subject = None, values = None):
 
     return (fill_me)
 
-def parse_args(args): # return argruments seperated into their different data types
-    return_type = {
-            "integers": [],
-            "strings": []
-            }
-    for elem in args:
-        try:
-            x = int(elem)
-            return_type['integers'].append(x)
-        except:
-            if elem.find(':') != -1: # We have a range
-                s = elem.split(":")
-                if len(s) == 2: # we have a correctly formatted range
-                    try:
-                        digit1 = int(s[0])
-                        digit2 = int(s[1])
-                        for n in range(digit1, digit2):
-                            return_type['integers'].append(n)
-                    except:
-                        print("failed.")
-                        pass;
 
-            else:
-                return_type['strings'].append(elem)
-
-    return return_type;
 
 with open(file_token, 'r') as file:      # *takes all of gector's code*
     token = file.readline(100).strip()
@@ -186,7 +90,7 @@ print("Bot token: " + token)
 bot = discord.Client()
 
 @bot.event  # must confirm the connection when it's done connecting
-sync def on_ready():
+async def on_ready():
     print("Connected!")
     print("Username: " + bot.user.name)
     print("-----------------")
@@ -195,57 +99,41 @@ sync def on_ready():
 async def on_message(message):
     data = 0;
     user = ioMod.json_handler()
+    prefix = "@"
     author = message.author.name.lower()
     messages = bot.messages
     helpmeh = help_meh()
     title = ""
 
-<<<<<<< HEAD
-=======
-    global msg_author
-    global msg_chan
-    global msg_content
-
->>>>>>> f212b8f9aa5f66a55e31e9644a527a95340a09d7
     msg_content = message.content
     msg_author = message.author.name.lower()
     msg_chan = message.channel
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f212b8f9aa5f66a55e31e9644a527a95340a09d7
-    def write_the_dang_thing():
-        if users[message.author.name.lower()] !=-1: # for the object in that user's file....
-            data[title].insert(0, usermessage.content) # inserts the message into the file
-            user.write(0, data, author + ".json") # writes the file
-
-    def
 
     if message.author.name.lower() == "scratchy":   # prevents triggering of self
         return
 
-    if msg_content.startswith(prefix + "test"):
-        parse_args(msg_content.split(' '))
-
-    ### `@show [field] [range]`
     if message.content.startswith(prefix + "show"):
-        args = parse_args(message.content.split(" ")[1:]) # Returns a dictionary of integers/strings with their indexes.
-        
+        message_args = message.content.split(" ")
+        arg1 = None
+        try:
+            arg1 = message_args[1:]
+        except Exception as e:
+            print("no argrument")
+
+        data = None;
         handler = ioMod.json_handler()
-        data = handler.read_user(msg_author) # Error handling of files is done in the class.
-        
-        if type(data) == str:   # Error message was returned
-            await bot.send_message(msg_chan, data)
-            return # Stop the code.
+        try:
+            data = handler.read_user(message.author.name.lower())
+        except Exception as e:
+            print("ERROR: Failed to read user '{}', does their file exist?".format(message.author.name.lower()))
+            await bot.send_message(message.channel, e)
+            return;
 
-        if len(args['strings']) == 0: ## Show only list of posssible fields
-            await bot.send_message(msg_chan, show(data = data)) # Code that calls the SHOW function
-        elif len(args['integers']) >= 1 and len(args['strings']) >= 1: ## Show either range or specified indexes of values
-            await bot.send_message(message.channel, show(data = data, subject = args['strings'], values = args['integers'])) 
-        elif len(args['strings']) >= 1: ## Show the values of that specific field
-            await bot.send_message(message.channel, show(data = data, subject = args['strings'])) 
-
+        if arg1 != "" or arg1 != None:
+            await bot.send_message(message.channel, show(data = data, subject = arg1))
+        else:
+            await bot.send_message(message.channel, show(data = data))
 
     if msg_content.startswith(prefix + "new"):
         message_args = msg_content.split(" ")
@@ -255,32 +143,11 @@ async def on_message(message):
             arg1 = str(message_args[1].strip())
         except Exception as e:
             await bot.send_message(msg_chan, "Err: Unable to parse args. Are you using `new <entry_name>`?") # Report error
-            await bot.send_message(msg_chan, "{}".format(e))
             return
-
-        result = new(arg=arg1)
-        await bot.send_message(msg_chan, result)
 
 
     if msg_content.startswith(prefix + "set"):
         # set <entry> <field> <value>
-        if(len(msg_content.split(' ')) <= 3):
-            await bot.send_message(msg_chan, "Incorrect amount or setup of argruments. Try `@set <entry> <field> <value>`.")
-            return;
-
-        args = parse_args(msg_content.split(" ")[1:])
-        
-        handler = ioMod.json_handler()
-        data = handler.read_user(msg_author)
-        if type(data) == str:   # Error message was returned
-            await bot.send_message(msg_chan, data)
-            return # Stop the code.
-
-        if len(args['strings']) >= 3: # We have a correct setup.
-            await bot.send_message(msg_chan, set_entry(args['strings'], data=data))
-        else:
-            await bot.send_message(msg_chan, "Incorrect amount or setup of argruments. Try `@set <entry> <field> <value>`.")
-        '''
         message_args = msg_content.split(" ")
         arg1, arg2, arg3 = None, None, None # Way of assignming multiple variables at once.
 
@@ -319,15 +186,6 @@ async def on_message(message):
         except Exception as e:
             await bot.send_message(msg_chan, "Err: Unable to parse data. '{}'".format(e))
             return
-        '''
-
-    if msg_content.startswith(prefix + "upload_scratch"):
-        if os.path.isfile(msg_author + ".json"):
-            await bot.send_message(msg_chan, "Uploading your scratchpad...")
-            await bot.send_file(msg_chan, os.getcwd() + "/" + msg_author + ".json")
-        else:
-            await bot.send_message(msg_chan, "Err: Unable to upload file, check to make sure it exists.")
-
 
 
     if msg_content.startswith(prefix + "upload_scratch"):
@@ -338,74 +196,84 @@ async def on_message(message):
             await bot.send_message(msg_chan, "Err: Unable to upload file, check to make sure it exists.")
 
 
-    '''
+
     if message.content.startswith(prefix + "grab"): # checks for the trigger command
         user = ioMod.json_handler
         data = user.read_user(0, author)
-<<<<<<< HEAD
-        arg1, arg2, arg3 = [0, 0, 0]
-        usr_prefix = "usr:"
-        title_prefix = "t:"
-        num_prefix = "num:"
-=======
-<<<<<<< HEAD
-        usr_prefix = "usr:"
-        title_prefix = "t:"
-        num_prefix = "num:"
-        messageargs = message.content.split(" ")
+        usr, title, num = [0, 0, 0]
+        usr_pre = "usr"
+        title_pre = "t"
+        num_pre = "num"
+        messageargs = msg_content.split(" ").split(";")
 
-        if message.content.lower().find(usr_prefix) !=-1:
-            
+        def write_the_dang_thing(result):
+            result = result_from
+            if num != 0:
+                i = num
+                while i > 0:
+                    if users[message.author.name.lower()] !=-1: 
+                        data[title].insert(0, result.content) # inserts the message into the file
+                        user.write(0, data, author + ".json") # writes the file
+                        i = i - 1
 
-
-
-        #        while i > 0:
-        #   messages.rotate(rotate)
-        #  usermessage = messages.pop()
-        #    write_the_dang_thing()
-        #    if i != 0:
-        #        x = rotate - 1
-        #        rotate = rotate * -1
-        #        messages.rotate(rotate)
-        #        rotate = x
-        #        messages.rotate(rotate)
-        #        i - 1
-        #    elif i == 0:
-        #        await bot.send_message(message.channel, "written to " + author + "'s scratchpad!")
-=======
-	arg1, arg2, arg3 = [0, 0, 0]
-        rotate = 0
-        before_rotate = 0
-        messages_from = 0
->>>>>>> f212b8f9aa5f66a55e31e9644a527a95340a09d7
-        messageargs = message.content.split(" ")
-        arg1 = messageargs[1].strip()
-        arg2 = messagesargs[2].strip()
-        arg3 = messageargs[3].strip()
-
-        if
-
-
-        # deque stuff
         # smaller deque to shuffle through
-        msg_deq = messages[:200]
-        channel = msg_chan # grabs channel to sort from
+        msg_deq = messages[:200] # makes deque 200 messages
+        channel = msg_chan # grabs channel to sort from (chan you sent trigger from)
 
-        chan_deq = filter(function, msg_deq)
+        def chan_cheq(input_arg): # sorts through messages from specific channels
+            if input_arg.channel == channel:
+                return input_arg
 
-        if message.content.lower().find("usr:"):
-            # read in after prefix
+        def usr_cheq(input_arg): # sorts through specific users
+            if usr == users:
+                return input_arg
 
-            usr_deq = filter(function, chan_deq)
+        #def grab_num():
+
+        chan_deq = filter(chan_cheq, msg_deq) # filters out specific channel to grab from in the deq
+
+        if title_pre in messageargs:
+            title_place = messageargs.index("t")
+            title_name = title_place + 1
+            title = messageargs[title_name].strip()
+            print(title)
+        else:
+            title = "messages"
+            print(title)
 
 
+        if usr_pre in messageargs:
+            usr_place = messageargs.index("usr") # returns where usr is
+            usr_name = usr_place + 1  # gets position of username
+            usr = messageargs[usr_name].strip() # gets user to copy from
+            print(usr)
+            usr_deq = filter(usr_cheq, chan_deq) # filters deq with username to copy from
+            result_from = usr_deq
 
-        # read in after usr
-        #if msg_content.startswith(usr_prefix):
-        #   for msg_content.lower().find(users):
+            if num_pre in messageargs:
+                num_place = messageargs.index("num") # finds number arg position
+                num_pos = num_place + 1 # gets actual number
+                num = messageargs[num_pos].strip()
+                print(int(num))
+            else:
+                num = 1
+                print(num)
+                write_the_dang_thing()
+                await bot.send_message(msg_chan, "Saved to " + author + "'s scratchpad!")
+                return
 
-
- #        await bot.send_message(message.channel, "written to " + author + "'s scratchpad!")
+        elif num_pre in messageargs: # copied bc if no user, can't pull from usr_deq
+            num_place = messageargs.index("num") # finds number arg position
+            num_pos = num_place + 1 # gets actual number
+            num = messageargs[num_pos].strip()
+            print(int(num))
+            result_from = chan_deq
+        else:
+            num = 1
+            print(num)
+            result_from = chan_deq
+        write_the_dang_thing()
+        await bot.send_message(msg_chan, "Saved to " + author + "'s scratchpad!")
 
     if message.content.startswith(prefix + "help"):
         await bot.send_message(message.channel, "What command would you like help with?")
@@ -413,6 +281,5 @@ async def on_message(message):
         print(helpmeh.with_grab())
         #await bot.send_message(message.channel, "test")
 
-    '''
-bot.run(token)
 
+bot.run(token)
